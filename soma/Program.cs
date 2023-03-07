885d72275a1de5cf8d5538c18e4c2c5b27465359
alfernab <86300297+alfernabreu@users.noreply.github.com>
2023-03-06 22:46:57 -0300

    soma code finished but not tested

--- soma/Program.cs
+++ soma/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace soma
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int i = 13;
+            int soma = 0;
+            int k = 0;
+
+            while (k < i)
+            {
+                k += 1;
+                soma += k;
+            }
+
+            Console.WriteLine(soma);
+        }
+    }
+}
