5ddc3152179badaba161c411900f0a2ba4fda36e
alfernab <86300297+alfernabreu@users.noreply.github.com>
2023-03-07 13:34:52 -0300

    input for the user decide to check another number created

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/* Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
+
+
+IMPORTANTE:
+
+Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código; */
+
+
+using System;
 
 namespace fibonacci
 {
@@ -6,31 +14,54 @@ namespace fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bem-vindo(a)!\nVamos checar se o número digitado pelo usuário pertence ao conjunto de valores que compõe a Sequência de Fibonacci.");
+            Console.WriteLine("\nBem-vindo(a)!\nVamos checar se o número digitado pelo usuário pertence ao conjunto de valores que compõe a Sequência de Fibonacci.");
 
-            int primeiro = 0;
-            int segundo = 1;
-            int terceiro = 1;
+            bool valido = false;
+            int numero;
+            string opcao;
+            string escolha = "sim";
+            do 
+            {
+                Console.Write("Informe um número inteiro: ");
+                numero = int.Parse(Console.ReadLine());
 
-            List<int> sequenciaFibonacci = new List<int>();
-            for (int i = 0; i < 50; i++)
-            {
-                sequenciaFibonacci.Add(segundo);
-                terceiro = segundo;
-                segundo = primeiro + segundo;
-                primeiro = terceiro;
-            }
+                int primeiro = 0;
+                int segundo = 1;
+                int terceiro = 1;
 
-            Console.Write("Informe um número: ");
-            int numero = int.Parse(Console.ReadLine());
-            if(sequenciaFibonacci.Contains(numero))
-            {
-                Console.WriteLine($"O número {numero} se encontra no conjunto de números que compõe a Sequência Fibonacci.");
-            }
-            else
-            {
-                Console.WriteLine($"O número {numero} não se encontra no conjunto de números que compõe a Sequência Fibonacci.");
-            }
+                List<int> sequenciaFibonacci = new List<int>();
+                for (int i = 0; i < 50; i++)
+                {
+                    sequenciaFibonacci.Add(segundo);
+                    terceiro = segundo;
+                    segundo = primeiro + segundo;
+                    primeiro = terceiro;
+                }
+
+                if(sequenciaFibonacci.Contains(numero))
+                {
+                    Console.WriteLine($"\nO número {numero} se encontra no conjunto de números que compõe a Sequência Fibonacci.");
+                }
+                else
+                {
+                    Console.WriteLine($"\nO número {numero} não se encontra no conjunto de números que compõe a Sequência Fibonacci.");
+                }
+
+                Console.Write("\nVocê deseja checar outro número? Digite \"Sim\" ou \"Não\": ");
+                opcao = Console.ReadLine();
+                escolha = opcao.ToLower();
+
+                while (escolha != "sim" & escolha != "não")
+                {
+                    Console.WriteLine("\nAtenção! Você digitou um valor inválido.");
+                    Console.Write("Por favor, digite \"Sim\" ou \"Não\" caso deseje checar outro número: ");
+                    opcao = Console.ReadLine();
+                    escolha = opcao.ToLower();
+                }
+
+            } while (escolha == "sim");
+
+            Console.WriteLine("\nObrigado pela escolha.");
         }
     }
 }
