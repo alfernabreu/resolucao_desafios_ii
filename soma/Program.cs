8052c8caf6cc14eece13e37d5d979ecae997e6de
alfernab <86300297+alfernabreu@users.noreply.github.com>
2023-03-07 10:35:07 -0300

    uploading files

--- soma/Program.cs
+++ soma/Program.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿// 1) Observe o trecho de código abaixo:
+
+// int INDICE = 13, SOMA = 0, K = 0;
+
+// enquanto K < INDICE faça
+
+// {
+
+// K = K + 1;
+
+// SOMA = SOMA + K;
+
+// }
+
+// imprimir(SOMA);
+
+
+
+// Ao final do processamento, qual será o valor da variável SOMA?
+
+
+using System;
 
 namespace soma
 {
