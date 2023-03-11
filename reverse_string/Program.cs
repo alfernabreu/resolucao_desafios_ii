7270795845e65bca16af6856cf21105e8efdfe2d
alfernab <86300297+alfernabreu@users.noreply.github.com>
2023-03-11 18:21:43 -0300

    method RetornaStringAoContrario and Main created

--- reverse_string/Program.cs
+++ reverse_string/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* reverse_string.cs
+
+Escreva um programa que inverta os caracteres de um string. */
+
+using System;
+
+namespace reverse_string
+{
+    class Program
+    {
+        private static void RetornaStringAoContrario(string palavra)
+        {
+            string palavraAoContrario = "";
+            int extensaoDaPalavra;
+
+            extensaoDaPalavra = palavra.Length - 1;
+
+            while(extensaoDaPalavra >= 0)
+            {
+                palavraAoContrario += palavra[extensaoDaPalavra];
+                extensaoDaPalavra --;
+            }
+
+            Console.WriteLine(palavraAoContrario);
+
+            Console.ReadLine();
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-vindo(a)\nO propósito deste aplicativo é apresentar a versão invertida de uma palavra ou frase informada pelo usuário");
+            Console.Write("Informe uma plavra ou frase para vê-la invertida: ");
+            palavra = Console.ReadLine();
+
+            RetornaStringAoContrario(palavra);
+
+        }
+
+
+    }
+}
+
+
