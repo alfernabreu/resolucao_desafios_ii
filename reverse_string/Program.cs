c71da0cc4a6dfc8c08226dae8e74a604f221fccb
alfernab <86300297+alfernabreu@users.noreply.github.com>
2023-03-11 19:20:05 -0300

    options for the user to decide to input another word was added

--- reverse_string/Program.cs
+++ reverse_string/Program.cs
@@ -22,18 +22,37 @@ namespace reverse_string
             }
 
             Console.WriteLine($"A expressão {palavra} em sua forma invertida é: {palavraAoContrario}");
-
-            Console.ReadLine();
         }
 
         public static void Main(string[] args)
         {
-            string novaPalavra;
+            string opcao;
+            string escolha;
             Console.WriteLine("Bem-vindo(a)\nO propósito deste aplicativo é apresentar a versão invertida de uma palavra ou frase informada pelo usuário");
-            Console.Write("Informe uma palavra ou frase para vê-la invertida: ");
-            String palavra = Console.ReadLine();
+            do
+            {
+                Console.Write("Informe uma palavra ou frase para vê-la invertida: ");
+                String palavra = Console.ReadLine();
 
-            RetornaStringAoContrario(palavra);
+                RetornaStringAoContrario(palavra);
+
+                Console.Write("\nDeseja ver outra palavra ou frase invertida? Digite \"Sim\" ou \"Não\": ");
+                opcao = Console.ReadLine();
+                escolha = opcao.ToLower();
+
+                while (escolha != "sim" & escolha != "não")
+                {
+                    Console.WriteLine("\nAtenção! Você digitou um valor inválido.");
+                    Console.Write("Por favor, digite \"Sim\" ou \"Não\" caso deseja checar outro número: ");
+                    opcao = Console.ReadLine();
+                    escolha = opcao.ToLower();
+                }
+
+            } while (escolha == "sim");
+
+            Console.WriteLine("\nObrigado pela escolha.\nPressione a tecla Enter para concluir.");
+
+            Console.ReadLine();
         }
     }
 }
